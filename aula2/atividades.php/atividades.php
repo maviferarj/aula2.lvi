316ededec3dfc2a394222bbce136d2d2e7786e8c
maviferarj <161755121+maviferarj@users.noreply.github.com>
2024-06-29 20:14:52 -0300

    Add files via upload

---
+++ aula2/atividades.php/atividades.php
@@ -0,0 +1,26 @@
+<?php
+    #variavel do tipo string.
+    $nome = "Maria vitória"; 
+
+    #variavel do tipo inteiro.
+    $idade = 17;
+
+    #variavel do tipo flutuante.
+    $altura = 1.68;
+
+    #variavel do tipo boleano.
+    $f1 = true;
+    $f2 = false;
+
+    #variavel nula
+    $nulo = null;
+    
+    #variavel array (vetor).
+    $matriz = array(1,2, 'abc',11,22);
+
+    echo '<pre>';
+    var_dump($nome, $idade, $altura, $f1, $f2, $nulo, $matriz);
+
+
+
+?>
---
+++ aula2/exercicios.php/exercicios.php
@@ -0,0 +1,8 @@
+<?php
+$num1 = 5;
+$num2 = 5;
+$soma = $num1 + $num2;
+
+echo "o resultado é: ($soma)";
+
+?>
